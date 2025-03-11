4e1d133f4af5c15248844a7d4788b230ce89806c
thialms <thiiago.allmeida44@gmail.com>
2025-03-11 19:35:32 -0300

    Stash changes before merging

--- escalonamento/src/components/Main/Main.jsx
+++ escalonamento/src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 const Main = () => {
   return (
     <div className="mt-10 flex h-[80vh] flex-col items-center justify-center rounded-2xl bg-[#6093A4]">
-      <div className="flex items-center justify-center space-x-4 mb-4">
+      <div className="mb-4 flex items-center justify-center space-x-4">
         <button className="cursor-pointer rounded-lg bg-[#F7B32B] px-4 py-2 text-xl font-semibold text-[#6093A4]">
           Duas Linhas
         </button>
@@ -10,7 +10,7 @@ const Main = () => {
         </button>
       </div>
 
-      <div className="m-4 grid grid-cols-3 gap-4 border-3 border-[#F7B32B] p-4 h-[20vh]">
+      <div className="border-3 m-4 grid h-[20vh] grid-cols-3 gap-4 border-[#F7B32B] p-4">
         <input type="text" className="border-[#F7B32B] p-2" placeholder="A1" />
         <input type="text" className="border-[#F7B32B] p-2" placeholder="A2" />
         <input type="text" className="border-[#F7B32B] p-2" placeholder="A3" />
